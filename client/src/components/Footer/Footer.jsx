349396a205a6203f5b72bb5ba4d798bcc4ec132b
gbyanova <gbyanova@gmail.com>
2025-03-22 01:41:38 +0200

    Add initial media queries

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import styles from './Footer.module.css';
 export default function Footer() {
     return (
         <p className={styles.bottomBar}>
-            <span>Всички права запазени</span>
+            <span className={styles.brand}>myBuilding</span>
             <span> | </span>
             <span>2025</span>
             <span> | </span>
-            <span>myBuilding</span>
+            <span>Всички права запазени</span>
         </p>
     )
 }
