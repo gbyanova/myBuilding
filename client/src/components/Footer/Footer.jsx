967c7e6db067a53cfba84b9303957f7bd008b037
gbyanova <gbyanova@gmail.com>
2025-03-22 00:29:13 +0200

    Add Footer file

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <p className="bottom-bar">
+            <span>All Rights Reserved</span>
+            <span> | </span>
+            <span>2025</span>
+            <span> | </span>
+            <span>myBuilding</span>
+        </p>
+    )
+}
