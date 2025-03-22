608a38a1ca1e12462505f4b7b61b6cc32e0a215b
gbyanova <gbyanova@gmail.com>
2025-03-22 15:49:03 +0200

    Adding Home Adding Login Adding Register Initial components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
+import Home from "./components/Home/Home"
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
+
+
 
 
 function App() {
 
     return (
+
         <div className="application">
+
             <Header />
+            <Home />
+            <Login />
+            <Register />
             <Footer />
+
         </div>
+
     )
 }
 
---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,28 @@
+
+
+export default function Login() {
+    return (
+
+
+        <section>
+
+            <form>
+                <h1>Вход</h1>
+                <div>
+                    <label>Имейл:</label>
+                    <input />
+
+                    <label>Парола:</label>
+                    <input />
+                    <input />
+                    <p>
+                        Ако нямате профил регистрирайте се <span>тук</span>
+                    </p>
+                </div>
+            </form>
+        </section>
+
+
+
+    )
+}
